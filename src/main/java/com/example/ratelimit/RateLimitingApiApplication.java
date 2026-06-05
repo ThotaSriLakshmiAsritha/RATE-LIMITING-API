2d95c1cc7f3cc15c55f507e334236048a7ce49be
Jathin <jathinyanna4@gmail.com>
2026-06-05 15:02:15 +0530

    final

---
+++ src/main/java/com/example/ratelimit/RateLimitingApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ratelimit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateLimitingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateLimitingApiApplication.class, args);
+	}
+
+}
